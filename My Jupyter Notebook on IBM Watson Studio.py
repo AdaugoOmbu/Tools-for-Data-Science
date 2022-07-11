f8469401664f778385777d011bef5f854071460f
AdaO <102874490+AdaugoOmbu@users.noreply.github.com>
2022-07-11 04:29:46 +0100

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **ADAUGO OMBU Data Scientist
+
+# *I am interested in Data Science because it gives meaning to raw data and converts it into meaningful insights that can be used to grow the business and recognize market trends
+
+# ### This code adds two numbers together
+
+# In[3]:
+
+
+a = 2
+b = 3
+print (a + b)
+
+
+# * The code works well.
+# * Its a simple code.
+# * It exceutes when run with no errors
+# 
+# ***
+# 
+# Please note formatting is done according to instructions and code published on githb profile below.
+# 
+# ***
+# 
+# <a href="https://github.com/AdaugoOmbu/Tools-for-Data-Science" target="_blank">Github profile</a>
+# 
+# 
+
+# In[ ]:
+
+
+
+
